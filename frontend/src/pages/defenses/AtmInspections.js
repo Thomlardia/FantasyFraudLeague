7992e9f726d2ad58eb3d58278587d296649758dc
Rekvelt, TQ, Mr [29941601@sun.ac.za] <29941601@sun.ac.za>
2025-08-14 16:55:22 +0200

    first interlink

--- frontend/src/pages/defenses/AtmInspections.js
+++ frontend/src/pages/defenses/AtmInspections.js
@@ -4,6 +4,14 @@ function AtmInspections() {
     return (
         <div>
             <h1>AtmInspections</h1>
+            <section>
+                <h2>Defends against:</h2>
+                <ul>
+                    <li>
+                        <Link to="/frauds/AtmSkimming">AtmSkimming</Link>
+                    </li>
+                </ul>
+            </section>s
             <Link to="/defenseshop">BACK</Link>
         </div>
     );
