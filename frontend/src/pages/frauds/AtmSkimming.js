60d0f457e318c1634f355aa8fc988e12a9f444e5
Rekvelt, TQ, Mr [29941601@sun.ac.za] <29941601@sun.ac.za>
2025-08-15 15:57:35 +0200

    Some more interlinks

--- frontend/src/pages/frauds/AtmSkimming.js
+++ frontend/src/pages/frauds/AtmSkimming.js
@@ -4,6 +4,14 @@ function AtmSkimming() {
     return (
         <div>
             <h1>AtmSkimming</h1>
+            <section>
+                <h2>How to Defend:</h2>
+                <ul>
+                    <li>
+                        <Link to="/defenses/TamperProofSeals">Tamper Proof Seals</Link>
+                    </li>
+                </ul>
+            </section>
             <Link to="/fraudwiki">BACK</Link>
         </div>
     );
