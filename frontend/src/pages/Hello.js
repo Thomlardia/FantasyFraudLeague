b13f868ba619e2e2842c52714a28216342949e7f
De Villiers, A, Mr [26936844@sun.ac.za] <26936844@sun.ac.za>
2025-08-18 21:27:48 +0200

    feat(User Interface): Styled Welcome screen & updated Home screen

--- frontend/src/pages/Hello.js
+++ frontend/src/pages/Hello.js
@@ -1,15 +1,30 @@
 import { Link } from 'react-router-dom';
+import '../styles/ui.css';
+import fflLogo from '../images/ffl_logo_ghost.png';
 
 function Hello() {
   return (
-    <div>
-      <h1>Fantasy Fraud League</h1>
-      <p>Welcome to the game.</p>
-      <Link to="login">LOGIN</Link>
-      <br />
-      <Link to="signup">SIGN UP</Link>
-      <br />
-      <Link to="adminlogin">ADMIN LOGIN</Link>
+    <div className="splash">
+      <div className="splash-header">
+        <img src={fflLogo} alt="FFL Shield Logo" className="splash-logo" />
+      </div>
+      
+      <div className="splash-welcome">WELCOME TO</div>
+      
+      <div className="splash-main-title">
+        <span>FANTASY</span>
+        <span>FRAUD</span>
+        <span>LEAGUE</span>
+      </div>
+      
+      <div className="splash-buttons">
+        <Link to="login" className="splash-button primary">LOGIN</Link>
+        <Link to="signup" className="splash-button secondary">SIGN UP</Link>
+      </div>
+      
+      <div className="splash-admin">
+        <Link to="adminlogin" className="splash-admin-link">ADMIN LOGIN</Link>
+      </div>
     </div>
   );
 }
