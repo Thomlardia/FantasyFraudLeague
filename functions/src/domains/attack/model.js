e6c25df5944af637ea021d6f7c6d734e690581c1
Rekvelt, TQ, Mr [29941601@sun.ac.za] <29941601@sun.ac.za>
2025-09-01 16:41:12 +0200

    added danger level to classes

--- functions/src/domains/attack/model.js
+++ functions/src/domains/attack/model.js
@@ -2,122 +2,142 @@
 // Each class represents a different fraud attack with a single attribute: damageDone
 
 class AccAndInvFraud {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class AccountTakeover {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class AtmSkimming {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class AuthPushPayments {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class BruteForce {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class BusinessEmailCompromise {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class CryptoJacking {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 1) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Ddos {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Deepfake {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class InsiderFraud {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class InvestmentScam {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class ManInTheMiddle {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Phishing {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Ransomware {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class SimSwap {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class SqlInjection {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class SyntIdentityTheft {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Vishing {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 2) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class Xss {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 1) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
 class ZeroDayExploit {
-  constructor(baseDamage) {
+  constructor(baseDamage, dangerLevel = 3) {
     this.baseDamage = baseDamage;
+    this.dangerLevel = dangerLevel;
   }
 }
 
