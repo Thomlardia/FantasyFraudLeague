d02e0468a313ae80ad78184967ee0890dfcd9b45
Rekvelt, TQ, Mr [29941601@sun.ac.za] <29941601@sun.ac.za>
2025-09-01 16:34:14 +0200

    Attack structure and classes

---
+++ functions/src/domains/attack/model.js
@@ -0,0 +1,145 @@
+// FraudAttack model file
+// Each class represents a different fraud attack with a single attribute: damageDone
+
+class AccAndInvFraud {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class AccountTakeover {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class AtmSkimming {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class AuthPushPayments {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class BruteForce {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class BusinessEmailCompromise {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class CryptoJacking {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Ddos {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Deepfake {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class InsiderFraud {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class InvestmentScam {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class ManInTheMiddle {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Phishing {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Ransomware {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class SimSwap {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class SqlInjection {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class SyntIdentityTheft {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Vishing {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class Xss {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+class ZeroDayExploit {
+  constructor(baseDamage) {
+    this.baseDamage = baseDamage;
+  }
+}
+
+module.exports = {
+  AccAndInvFraud,
+  AccountTakeover,
+  AtmSkimming,
+  AuthPushPayments,
+  BruteForce,
+  BusinessEmailCompromise,
+  CryptoJacking,
+  Ddos,
+  Deepfake,
+  InsiderFraud,
+  InvestmentScam,
+  ManInTheMiddle,
+  Phishing,
+  Ransomware,
+  SimSwap,
+  SqlInjection,
+  SyntIdentityTheft,
+  Vishing,
+  Xss,
+  ZeroDayExploit
+};
